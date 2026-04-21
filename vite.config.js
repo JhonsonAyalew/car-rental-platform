c892a71a121a37e9c3907414694b386231122af5
Jhonson Aylew <152905908+JhonsonAyalew@users.noreply.github.com>
2026-04-21 01:35:04 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',                    // ← Add this line
+  server: {
+    port: 3000,                 // optional: you can change port if you want
+  },
 })
